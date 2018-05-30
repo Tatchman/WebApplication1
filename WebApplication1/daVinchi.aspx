725ad1cdcc7ed843f821fcba5c5b7ed6e8f3a424
Jake Tatchell <jj.tatchell@virgin.net>
2018-05-30 17:53:30 +0100

    update COP 30/05/18

---
+++ WebApplication1/daVinchi.aspx
@@ -0,0 +1,8 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="daVinchi.aspx.cs" Inherits="WebApplication1.daVinchi" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <br />
+    <br />
+    <asp:GridView ID="GridView1" runat="server"></asp:GridView>
+</asp:Content>
---
+++ WebApplication1/daVinchi.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.OleDb;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class daVinchi : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                string cs = ConfigurationManager.ConnectionStrings["restaurantConnectionString"].ConnectionString;
+                using(OleDbConnection con = new OleDbConnection(cs))
+                {
+                    OleDbCommand cmd = new OleDbCommand("Select * from meals", con);
+                    con.Open();
+                    GridView1.DataSource = cmd.ExecuteReader();
+                    GridView1.DataBind();
+                }
+            }
+            catch (Exception)
+            {
+                Response.Redirect("errorpage.aspx");
+            }
+        }
+    }
+}
