6a49e99b05c96de2b5c442b634314e2c2d49e96d
Jake Tatchell <jj.tatchell@virgin.net>
2018-05-30 17:19:54 +0100

    Update 17:00

--- WebApplication1/Contact.aspx
+++ WebApplication1/Contact.aspx
@@ -3,7 +3,7 @@
     <div id="containerBox" style="display:inline;">
         <br />
         <div id="leftDiv" style="float:left">
-            <iframe src="https://www.google.com/maps enterURL here" width="400" height="400" frameborder="0" style="border:0" allowfullscreen></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d10054.52159106645!2d-2.637528!3d50.949048!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xe18d316b641b265b!2sYeovil+College!5e0!3m2!1sen!2suk!4v1527696561162" width="400" height="400" frameborder="0" style="border:0" allowfullscreen></iframe>
             <br /><small><a href="https://goo.gl/maps/j6dcdR7AsYK2" style="color:0000FF;text-align:left">View Larger Map</a></small>
         </div>
         <div id="rightDiv" style="float:right">
--- WebApplication1/Contact.aspx.cs
+++ WebApplication1/Contact.aspx.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Web.Mail;
+using System.Data;
+using System.Net.Mail;
+using System.Text;
 
 namespace WebApplication1
 {
@@ -12,6 +16,42 @@ namespace WebApplication1
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void btnReset_Click(object sender, EventArgs e)
+        {
+            txtMail.Text = "";
+            txtName.Text = "";
+            txtMessage.Text = "";
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            //Code 1 start
+            const string ToAddress = "Jake@gmail.com";
+            SmtpClient client = new SmtpClient();
+            client.Port = 587;
+            client.Host = "smtp.gmail.com";
+            client.EnableSsl = true;
+            client.Timeout = 10000;
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.UseDefaultCredentials = false;
+            client.Credentials = new System.Net.NetworkCredential("Jake@gmail.com", "password");
+
+            try
+            {
+                System.Net.Mail.MailMessage mm = new System.Net.Mail.MailMessage(txtMail.Text, ToAddress);
+                mm.Body = txtMessage.Text;
+                mm.Subject = "Online Test";
+                mm.BodyEncoding = UTF8Encoding.UTF8;
+                mm.DeliveryNotificationOptions = DeliveryNotificationOptions.OnFailure;
+                client.Send(mm);
+            }
+            catch (Exception ex)
+            {
+                lblError.Text = ex.Message;
+            }
+            //Code 1 End
         }
     }
 }
